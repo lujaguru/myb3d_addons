9b9fa5875461268786a89837495e7777c1fef4fd
Aditia A. Pratama <aditia.ap@gmail.com>
2013-03-08 22:20:26 +0800

    Create subsurf_toggle.py
    my first commit yeaay

---
+++ subsurf_toggle.py
@@ -0,0 +1,50 @@
+bl_info = {
+    "name": "Subsurf Viewport Toggle",
+    "description": "Adds toggle button in property region for subsurf viewport visibility",
+    "author": "Aditia A. Pratama, Greg Zaal",
+    "version": (0, 1),
+    "blender": (2, 66, 1),
+    "location": "3D View > Property Region (N-key)",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "3D View"}
+
+import bpy
+
+class SubsurfToggle(bpy.types.Operator):
+    'Toggle subsurf visibility'
+    bl_idname='subsurf.toggle'
+    bl_label='Subsurf Toggle'
+       
+    def execute(self,context):
+        state = bpy.context.active_object.modifiers['Subsurf'].show_viewport
+        for e in bpy.context.selected_objects:
+            try:
+                if state==False:
+                    e.modifiers['Subsurf'].show_viewport = True
+                else:
+                    e.modifiers['Subsurf'].show_viewport = False
+            except KeyError:
+                print ("No subsurf on "+e.name+" or it is not named Subsurf")
+        return {'FINISHED'}
+            
+class Visibility(bpy.types.Panel):
+    bl_label = "Subsurf Viewport"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+       
+    def draw (self, context):
+        layout=self.layout
+            
+        col=layout.column() 
+        col.operator("subsurf.toggle",  text="On/Off", icon="MOD_SUBSURF")
+
+def register():
+   bpy.utils.register_module(__name__)
+   
+def unregister():
+    bpy.utils.unregister_module(__name__)
+ 
+if __name__ == "__main__":
+    register()
