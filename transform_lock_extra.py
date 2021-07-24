21a7b4defaaf4da11fad1898ef048766f185b310
lujaguru <85117493+lujaguru@users.noreply.github.com>
2021-07-25 02:59:46 +0545

    Update transform_lock_extra.py
    updated to blender version 2.93

--- transform_lock_extra.py
+++ transform_lock_extra.py
@@ -3,7 +3,7 @@ bl_info = {
     "description": "Adds toggle button in property region for enabling lock transform to all selected object",
     "author": "Aditia A. Pratama",
     "version": (0, 1),
-    "blender": (2, 66,),
+    "blender": (2, 93,),
     "location": "3D View > Property Region (N-key)",
     "warning": "",
     "wiki_url": "",
@@ -13,7 +13,7 @@ bl_info = {
 import bpy
 
 ## Operator ## 
-class locZ_lock(bpy.types.Operator):
+class JNU_OT_locZ_lock(bpy.types.Operator):
     'Location Z Lock'
     bl_idname='locz.toggle'
     bl_label='Lock Loc Z'     
@@ -30,7 +30,7 @@ class locZ_lock(bpy.types.Operator):
                 print ("No lock identified")
         return {'FINISHED'}
 
-class locY_lock(bpy.types.Operator):
+class JNU_OT_locY_lock(bpy.types.Operator):
     'Location Y Lock'
     bl_idname='locy.toggle'
     bl_label='Lock Loc Y'     
@@ -47,7 +47,7 @@ class locY_lock(bpy.types.Operator):
                 print ("No lock identified")
         return {'FINISHED'}
     
-class locX_lock(bpy.types.Operator):
+class JNU_OT_locX_lock(bpy.types.Operator):
     'Location X Lock'
     bl_idname='locx.toggle'
     bl_label='Lock Loc X'     
@@ -64,7 +64,7 @@ class locX_lock(bpy.types.Operator):
                 print ("No lock identified")
         return {'FINISHED'}
     
-class rotZ_lock(bpy.types.Operator):
+class JNU_OT_rotZ_lock(bpy.types.Operator):
     'Rotation Z Lock'
     bl_idname='rotz.toggle'
     bl_label='Lock Rot Z'     
@@ -81,7 +81,7 @@ class rotZ_lock(bpy.types.Operator):
                 print ("No lock identified")
         return {'FINISHED'}
 
-class rotY_lock(bpy.types.Operator):
+class JNU_OT_rotY_lock(bpy.types.Operator):
     'Rotation Y Lock'
     bl_idname='roty.toggle'
     bl_label='Lock Rot Y'     
@@ -98,7 +98,7 @@ class rotY_lock(bpy.types.Operator):
                 print ("No lock identified")
         return {'FINISHED'}
     
-class rotX_lock(bpy.types.Operator):
+class JNU_OT_rotX_lock(bpy.types.Operator):
     'Rotation X Lock'
     bl_idname='rotx.toggle'
     bl_label='Lock Rot X'     
@@ -114,8 +114,8 @@ class rotX_lock(bpy.types.Operator):
             except KeyError:
                 print ("No lock identified")
         return {'FINISHED'}
-    
-class scaleZ_lock(bpy.types.Operator):
+
+class JNU_OT_scaleZ_lock(bpy.types.Operator):
     'Scale Z Lock'
     bl_idname='scalez.toggle'
     bl_label='Lock scale Z'     
@@ -132,7 +132,7 @@ class scaleZ_lock(bpy.types.Operator):
                 print ("No lock identified")
         return {'FINISHED'}
     
-class scaleY_lock(bpy.types.Operator):
+class JNU_OT_scaleY_lock(bpy.types.Operator):
     'Scale Y Lock'
     bl_idname='scaley.toggle'
     bl_label='Lock scale Y'     
@@ -149,7 +149,7 @@ class scaleY_lock(bpy.types.Operator):
                 print ("No lock identified")
         return {'FINISHED'}
  
-class scaleX_lock(bpy.types.Operator):
+class JNU_OT_scaleX_lock(bpy.types.Operator):
     'Scale X Lock'
     bl_idname='scalex.toggle'
     bl_label='Lock scale X'     
@@ -166,12 +166,14 @@ class scaleX_lock(bpy.types.Operator):
                 print ("No lock identified")
         return {'FINISHED'}
     
+    
 ## UI ##
 
-class TransformLockExtra(bpy.types.Panel):
+class TransformLockExtra_PT_Panel(bpy.types.Panel):
     bl_label = "Transform Locks Extra"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
+    bl_category = "Lock xyz"
        
     def draw (self, context):
         layout=self.layout
@@ -200,12 +202,30 @@ class TransformLockExtra(bpy.types.Panel):
         row.operator("scalex.toggle", text="X")
         row.operator("scaley.toggle", text="Y")
         row.operator("scalez.toggle", text="Z")
-        
-def register():
-   bpy.utils.register_module(__name__)
-   
-def unregister():
-    bpy.utils.unregister_module(__name__)
- 
+
+
+classes= [
+JNU_OT_locZ_lock,
+JNU_OT_locY_lock,
+JNU_OT_locX_lock,  
+JNU_OT_rotZ_lock,
+JNU_OT_rotY_lock,
+JNU_OT_rotX_lock,
+JNU_OT_scaleZ_lock,
+JNU_OT_scaleY_lock,
+JNU_OT_scaleX_lock,
+TransformLockExtra_PT_Panel
+]   
+    
+def register(): 
+    for c in classes:
+        bpy.utils.register_class(c)
+
+    
+def unregister(): 
+    for c in classes:
+        bpy.utils.unregister_class(c)
+
 if __name__ == "__main__":
     register()
+
